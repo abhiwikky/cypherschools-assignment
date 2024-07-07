aa0de26d9e957f16da18bbd86a2d4de02f11a9e3
Abhijith S <113137870+abhiwikky@users.noreply.github.com>
2024-07-07 19:03:01 +0530

    Add files via upload

---
+++ Lecture40-Cipherschools.cpp
@@ -0,0 +1,22 @@
+
+/*#include<iostream>
+using namespace std;
+int main(){
+
+    int array[5] = {23,355,23,21,3};
+    //arrays are the simplest data structures
+    return 0;
+
+}
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    //algorithm is logical thinking/solution to proceed towards a problem
+    int n =23;
+    int x = n*4;
+    int y = n+n+n+n;
+    cout<<x<<endl<<y;//both will give same solution but both have there different procedure
+    // X is much efficient
+    return 0;
+}
